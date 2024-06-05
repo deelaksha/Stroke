c97efc449a89ee628fdcdc9c512780ca0c6c922b
Paranjay-pandu <paranjaypandu133@gmail.com>
2024-06-05 08:27:50 +0530

    added exercises to the main page

--- index.php
+++ index.php
@@ -49,7 +49,8 @@
                             <div class="col-xl-7 col-lg-8 col-md-10 ">
                              <div class="hero-wrapper">
                              
-                    
+                              <a href="exercises.php" class="btn" data-animation="fadeInLeft" data-delay=".3s">Video Tutorials</a>
+                                </div>
                                 <div class="hero__caption">
                                     <h1 data-animation="fadeInUp" data-delay=".3s">Health is wealth  keep it healthy </h1>
                                     <a href="patient_login.php" class="btn" style="margin-left: 10px;">Patient Login</a>
@@ -65,19 +66,7 @@
                 <div class="container">
                     <div class="row align-items-center">
                         <div class="col-xl-7 col-lg-8 col-md-10 ">
-                         <div class="hero-wrapper">
-                            <!-- Video icon -->
-                            <div class="video-icon">
-                                <a class="popup-video btn-icon" href="https://www.youtube.com/watch?v=up68UAfH0d0" data-animation="bounceIn" data-delay=".4s">
-                                    <i class="fas fa-play"></i>
-                                </a>
-                            </div>
-                            <div class="hero__caption">
-                                <h1 data-animation="fadeInUp" data-delay=".3s">Health is wealth  keep it healthy </h1>
-                                <p data-animation="fadeInUp" data-delay=".6s">Almost before we knew it, we<br> had left the ground</p>
-                                <a href="#" class="btn" data-animation="fadeInLeft" data-delay=".3s">Take a Service</a>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
             </div>
